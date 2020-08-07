972dff4ade07e979b15fe9370b5d8becd27b9af4
Mark James Adams <mark.adams@ed.ac.uk>
2020-08-07 20:17:23 +0100

    Print info on each step of database creation process

--- db.sh
+++ db.sh
@@ -3,10 +3,20 @@
 # Create database schema
 if [ -f healthoutcomes.db ]; then rm healthoutcomes.db; fi
   
-# Load raw data
+echo "Creating import schema"
 sqlite3 healthoutcomes.db < raw.sql
+
+echo "Importing raw data"
 sqlite3 healthoutcomes.db < import.sql
+
+echo "Structuring GP records"
 sqlite3 healthoutcomes.db < gp.sql
+
+echo "Structuring HES records"
 sqlite3 healthoutcomes.db < hesin.sql
+
+echo "Creating end user views"
 sqlite3 healthoutcomes.db < views.sql
+
+echo "Indexing tables"
 sqlite3 healthoutcomes.db < index.sql

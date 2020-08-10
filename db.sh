0589051d615ae40ad5680a318eda8cc2f98e3204
Mark James Adams <mark.adams@ed.ac.uk>
2020-08-10 10:48:28 +0100

    Move SQL files into schema subdirectory

--- db.sh
+++ db.sh
@@ -4,22 +4,22 @@
 if [ -f healthoutcomes.db ]; then rm healthoutcomes.db; fi
   
 echo "Creating import schema"
-sqlite3 healthoutcomes.db < raw.sql
+sqlite3 healthoutcomes.db < schema/raw.sql
 
 echo "Importing raw data"
-sqlite3 healthoutcomes.db < import.sql
+sqlite3 healthoutcomes.db < schema/import.sql
 
 echo "Structuring GP records"
-sqlite3 healthoutcomes.db < gp.sql
+sqlite3 healthoutcomes.db < schema/gp.sql
 
 echo "Structuring HES records"
-sqlite3 healthoutcomes.db < hesin.sql
+sqlite3 healthoutcomes.db < schema/hesin.sql
 
 echo "Creating end user views"
-sqlite3 healthoutcomes.db < views.sql
+sqlite3 healthoutcomes.db < schema/views.sql
 
 echo "Indexing tables"
-sqlite3 healthoutcomes.db < index.sql
+sqlite3 healthoutcomes.db < schema/index.sql
 
 echo "Analysing database"
-sqlite3 healthoutcomes.db < analyse.sql
+sqlite3 healthoutcomes.db < schema/analyse.sql
